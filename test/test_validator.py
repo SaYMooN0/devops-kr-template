105f83cdf7be785cc9e77120b883019c13b2d710
SaYMooN <113824965+SaYMooN0@users.noreply.github.com>
2026-09-23 22:01:13 +0300

    feat: add phone validation
    * feat add validate_snils
    
    * feat: add phone validation function
    
    ---------
    
    Co-authored-by: cozydnb <dmgimatdinov@gmail.com>

--- test/test_validator.py
+++ test/test_validator.py
@@ -1,4 +1,22 @@
 # tests/test_validator.py
+from validator import validate_email, validate_phone, validate_snils
+
+
 def test_validate_email():
     assert validate_email("test@example.com") == True
     assert validate_email("invalid") == False
+
+
+def test_validate_phone():
+    assert validate_phone("+79991234567") == True
+    assert validate_phone("89991234567") == False
+    assert validate_phone("+7999123") == False
+
+
+def test_validate_snils():
+    assert validate_snils("11223344595") == True
+    assert validate_snils("001-001-999 65") == True
+    assert validate_snils("123") == False
+    assert validate_snils("123456789012") == False
+    assert validate_snils("abcdefghijk") == False
+    assert validate_snils("11223344500") == False
